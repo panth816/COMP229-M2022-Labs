b4818f04ada209e9ebe6f2fc0233dc67a90c4307
Panth Patel <panth816@gmail.com>
2022-06-04 17:42:55 -0400

    converted server to express

--- Week3/server.js
+++ Week3/server.js
@@ -1,42 +1,15 @@
 "use strict";
-const http = require('http');
-const fs = require('fs');
-const mime = require('mime-types');
-let lookup = mime.lookup;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
-const server = http.createServer(function (req, res) {
-    let path = req.url;
-    if (path === "/" || path === "/home") {
-        path = "/index.html";
-    }
-    let mime_type = lookup(path.substring(1));
-    console.log(`MIME TYPE: ${mime_type}`);
-    fs.readFile(__dirname + path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            console.log(`Error: ${err.message}`);
-            return res.end(`
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>ERROR - 404</title>
-            </head>
-            <body>
-                <h1>ERROR - 404 - File Not Found!</h1>
-            </body>
-            </html>
-            `);
-        }
-        res.setHeader("X-Content-Type-Options", "nosniff");
-        res.writeHead(200, { 'Content-Type': mime_type });
-        console.log(`Full File Name: ${__dirname}${req.url}`);
-        return res.end(data);
-    });
+app.get('/', function (req, res) {
+    res.send('Hello, World!');
 });
-server.listen(port, function () {
-    console.log(`Server running at Port: ${port}`);
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}`);
 });
 //# sourceMappingURL=server.js.map
