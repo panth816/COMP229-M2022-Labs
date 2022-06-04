8e5afb3fdae131d243ad809c55ecaab7734d38c6
Panth Patel <panth816@gmail.com>
2022-06-04 15:25:55 -0400

    Added typescript support

--- Week3/server.ts
+++ Week3/server.ts
@@ -1,16 +1,25 @@
-"use strict";
-const http = require('http');
-const fs = require('fs');
-const mime = require('mime-types');
-let lookup = mime.lookup;
+// require built-in modules
+import http from 'http'; // ECMAScript Module Pattern (ESM)
+import fs from 'fs'; 
+import mime from 'mime-types';
+
+let lookup = mime.lookup; //alias for mime.lookup
+
 const port = 3000;
-const server = http.createServer(function (req, res) {
-    let path = req.url;
-    if (path === "/" || path === "/home") {
+
+//When we create a server instance, it is IMMUTABLE(it cannot be changed until the server is restarted)
+const server = http.createServer(function(req, res)   
+{
+    let path = req.url as string; //alias for req.url
+
+    if(path === "/" || path === "/home")
+    {
         path = "/index.html";
     }
-    let mime_type = lookup(path.substring(1));
+
+    let mime_type = lookup(path.substring(1)) as string;
     console.log(`MIME TYPE: ${mime_type}`);
+
     fs.readFile(__dirname + path, function (err, data) {
         if (err) {
             res.writeHead(404);
@@ -36,7 +45,8 @@ const server = http.createServer(function (req, res) {
         return res.end(data);
     });
 });
-server.listen(port, function () {
+
+server.listen(port, function() 
+{
     console.log(`Server running at Port: ${port}`);
 });
-//# sourceMappingURL=server.js.map
